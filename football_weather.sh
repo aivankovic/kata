f64cecb9578993ef9ecb11b91589913e1261c546
Aleksandar.Ivankovic <aleksandar.ivankovic@gmail.com>
2015-10-18 18:29:20 +0200

    common file not with latest code

--- football_weather.sh
+++ football_weather.sh
@@ -13,7 +13,7 @@ while getopts "wf" opt; do
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
-      echo "Usage:" >&2
+      echo "Valid options:" >&2
       echo "	-w file   : weather  file parsing" >&2
       echo "	-f file   : football file parsing" >&2
       exit
@@ -29,19 +29,12 @@ fi
 if [ "$WEATHER" == 1 ]
 then
 awk '
-function abs(value)
-{
- return (value<0?-value:value);
-}
+$1 ~ /^[0-9]/  {diff=$2-$3
+                tempdiff[$1]=diff
+                }
+END {for (i in tempdiff) print i,tempdiff[i]}
 
-$1 ~ /^[0-9]/  {diff=abs($9-$7)
-		tempdiff[$1]=diff
-		team[$1]=$2
-		}
-
-END {for (i in tempdiff) print i,tempdiff[i], team[i]}
-
-' $inputfile | sort -nk 2 | head -1 |  cut -d ' ' -f 3
+' $inputfile | sort -nk 2 | head -1
 
 
 elif [ "$FOOTBALL" == 1 ]
--- weather.sh
+++ weather.sh
@@ -2,10 +2,8 @@
 
 
 awk '
-$1 ~ /^[0-9]/  {#print $2, $3
-		diff=$2-$3
+$1 ~ /^[0-9]/  {diff=$2-$3
 		tempdiff[$1]=diff
-		#print diff 
 		}
 END {for (i in tempdiff) print i,tempdiff[i]}
 
